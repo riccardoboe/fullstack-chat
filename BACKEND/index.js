dab72711862bba114e597a7f2e18251d275ca2ae
riccardoboe <108944237+riccardoboe@users.noreply.github.com>
2023-04-27 20:31:56 -0500

    Add files via upload

---
+++ BACKEND/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const axios = require("axios");
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "bcbcf61f-b81d-4290-8035-495854db2ff5" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
